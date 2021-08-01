b0eba0220a4276b2a0be34856795a6669a2423d0
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-01 13:30:22 -0700

    added post models and routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require('./User');
 const Post = require("./Post")
 
+User.hasMany(Post, {
+    foreignKey: "user_id"
+})
+
+Post.belongsTo(User, {
+    foreign_key:"user_id"
+})
+
 module.exports = { User, Post };
