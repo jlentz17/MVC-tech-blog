fc2934b0d214e3d69a1133d33b105bb0765cd280
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-02 00:18:41 -0700

    added post route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,4 +1,4 @@
-const sequelize = require("../../config/connection") 
+const sequelize = require("../../config/connection");
 const router = require("express").Router();
 const { Post, User, Vote } = require("../../models");
 
@@ -32,86 +32,102 @@ router.get("/:id", (req, res) => {
         attributes: ["username"],
       },
     ],
-  }).then((dbPostData) => {
-    if (!dbPostData) {
-      res.status(404).json({ message: "No post found with this id" });
-      return;
-    }
-    res.json(dbPostData);
   })
-  .catch(err => {
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
       console.log(err);
-      res.status(500).json(err)
-  })
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
-    Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        user_id: req.body.user_id
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    user_id: req.body.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.put("/upvote", (req, res) => {
-Vote.create({
-  user_id: req.body.user_id,
-  post_id: req.body.post_id
-})
-.then(dbPostData => res.json(dbPostData))
-.catch(err => 
-  res.json(err)
-)
-})
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  }).then(() => {
+    return Post.findOne({
+      where: {
+        id: req.body.post_id,
+      },
+      attributes: [
+        "id",
+        "title",
+        "created_at"[
+          (sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count")
+        ],
+      ],
+    })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
+});
 
 router.put("/:id", (req, res) => {
-    Post.update(
+  Post.update(
     {
-        title: req.body.title
+      title: req.body.title,
     },
     {
-        where: {
-            id: req.params.id
-        }
+      where: {
+        id: req.params.id,
+      },
     }
-    )
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: "No post found with this id" });
-            return;
-        }
-        res.json(dbPostData)
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
-
-
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.delete("/:id", (req, res) => {
-    Post.destroy({
-        where: {
-            id: req.params.id
-        }
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
     })
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: "No post found with this id" })
-            return;
-        }
-        res.json(dbPostData)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
