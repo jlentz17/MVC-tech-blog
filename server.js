cc95c1038b0052979c32d966625d8cc5de7c1b6a
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-08-05 16:27:47 -0700

    added helpers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const session = require("express-session");
+const helpers = require("./utils/helpers")
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -17,7 +18,7 @@ const sess = {
 };
 
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
